5f17df0cddfcc6cb7c3eae38e287640cf4508f6d
jinl33 <jinl33@nycstudents.net>
2025-11-13 14:07:31 -0500

    COunt 1-15 not finish

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,7 @@
 
-// CREATE EVENT LISTENERS
 
 
-
-
-
-
+let Loop_1_To_15_Result = document.querySelector("#Loop_1_To_15_Result");
 
 
 
@@ -13,3 +9,24 @@
 
 
 // CREATE FUNCTIONS W/ LOOPS
+
+function Loop_1_To_15() {
+    Loop_1_To_15_Result.classList.remove("hint");
+    for (let i = 1; i <= 15; i++){   
+        let HTML = `<span id="Result_Loop_1_To_15_${i}" class="chip">${i}</span>`;
+        let Number_DOM = Loop_1_To_15_Result.insertAdjacentHTML("beforeend", HTML)
+        
+        console.log(Number_DOM);
+
+        setTimeout( () => {
+            document.querySelector(`#Result_Loop_1_To_15_${i}`).classList.add("show");
+        },i * 100)
+
+    }
+}
+
+
+// CREATE EVENT LISTENERS
+
+document.querySelector("#Loop_1_To_15_BTN").addEventListener("click", () => Loop_1_To_15());
+
